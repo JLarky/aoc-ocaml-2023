08eeb0deae69ddfc4b44ece3834ea05d5ccb5b27
JLarky <jlarky@gmail.com>
2023-12-01 22:43:22 -0700

    oof

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Advent of Code 2023 in OCaml
+
+## Install OCaml
+
+(not easy)
+
+- [ocaml docs](https://ocaml.org/)
+
+## Usage
+
+```bash
+dune exec -w aoc2023
+```
--- aoc2023/bin/main.ml
+++ aoc2023/bin/main.ml
@@ -1,1 +1,13 @@
-let () = print_endline "Hello, World!"
+module H = struct
+  type t = {
+    (* *)
+    tag : string;
+    props : (string * string) list;
+    children : t list;
+  }
+
+  let h  = 123
+  let run = () = Printf.printf "Hello, world!\n"
+end
+
+let () = H.run()
